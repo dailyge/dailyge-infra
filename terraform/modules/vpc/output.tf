b5dd37ce4765318fad5d1d52d65b0ae5ebf8412e
devjun10 <devjun1023@gmail.com>
2024-06-27 03:23:05 +0900

    feat: vpc 프로비저닝 코드 작성

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.dailyge_vpc.id
+}
+
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.dailyge_public_subnets : subnet.id]
+}
+
+output "private_subnet_ids" {
+  value = {for subnet_id, subnet in aws_subnet.dailyge_private_subnets : subnet_id => subnet.id}
+}
