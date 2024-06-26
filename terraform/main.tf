2e75f859a1f62ce56386b645824062dbdb76f72c
devjun10 <devjun1023@gmail.com>
2024-06-27 03:22:29 +0900

    feat: Root main.tf, variables.tf, Provider 추가

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  cidr            = var.cidr
+  name            = var.name
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  tags            = var.tags
+}
+
+module "alb" {
+  source                = "./modules/alb"
+  name                  = var.name
+  public_subnets_ids    = module.vpc.public_subnet_ids
+  alb_security_group_id = module.alb.alb_security_group_id
+  vpc_id                = module.vpc.vpc_id
+  tags                  = var.tags
+}
+
+module "s3" {
+  source                      = "./modules/s3"
+  bucket_name                 = module.s3.bucket_name
+  domain_name                 = "www.dailyge.com"
+  cloudfront_distribution_arn = module.cloudfront.distribution_arn
+  tags                        = var.tags
+}
+
+module "cloudfront" {
+  source                         = "./modules/cloudfront"
+  bucket_id                      = module.s3.bucket_id
+  bucket_name                    = module.s3.bucket_name
+  s3_bucket_regional_domain_name = var.s3_bucket_regional_domain_name
+  cnames                         = var.cnames
+  acm_certificate_arn            = var.acm_certificate_arn
+  tags                           = var.tags
+}
