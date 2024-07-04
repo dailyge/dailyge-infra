c72e875e6820254ca51fb1bb785133ac0e99b6de
devjun10 <devjun1023@gmail.com>
2024-07-05 05:58:02 +0900

    refactor: 사용하지 않는 zone_id 삭제

--- terraform/dev/modules/route53/outputs.tf
+++ terraform/dev/modules/route53/outputs.tf
@@ -1,8 +1,3 @@
-output "route53_zone_id" {
-  value       = aws_route53_zone.dailyge_route53.zone_id
-  description = "The ID of the Route 53 hosted zone."
-}
-
 output "custom_ns_records" {
   value       = var.ns_records
   description = "Custom NS records provided for documentation purposes."
