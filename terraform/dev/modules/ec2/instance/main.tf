1fee7bb2734ef1bcf2d9df555852ecae703e3bee
Jun <92818747+devjun10@users.noreply.github.com>
2024-07-08 15:52:59 +0900

    [DLG-66] 인스턴스 프로비저닝에 적용된 불필요한 명령어를 삭제한다. (#14)
    * refactor: 불필요한 인스턴스 프로비저닝 코드 삭제
    - 사용하지 않는 명령어 삭제
    
    * style: 변수 정리
    - var, module 순서 고려

--- terraform/dev/modules/ec2/instance/main.tf
+++ terraform/dev/modules/ec2/instance/main.tf
@@ -5,20 +5,6 @@ resource "aws_instance" "dailyge_redis_instance" {
   subnet_id              = var.redis_subnet_id
   vpc_security_group_ids = var.redis_security_group_ids
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install docker -y
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo usermod -a -G docker ec2-user
-
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-
-    sudo docker run -d --name redis -p 6379:6379 redis:latest
-  EOF
-
   tags = {
     Name = "dailyge-redis"
   }
