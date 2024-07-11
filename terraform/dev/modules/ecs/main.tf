278d5a984ff3adfefade07a17a5461b83f1325ef
devjun10 <devjun1023@gmail.com>
2024-07-11 15:13:40 +0900

    refactor: EC2 port 22 개방 - SSH 접속을 위한 포트 개방

--- terraform/dev/modules/ecs/main.tf
+++ terraform/dev/modules/ecs/main.tf
@@ -55,6 +55,14 @@ resource "aws_security_group" "ecs_security_group" {
   vpc_id = var.vpc_id
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH port."
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
